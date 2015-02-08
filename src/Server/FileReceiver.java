d2e82e1635a4142d6c96e1d834405f09b7409d3c
Amra Poprzanovic <amrapop@gmail.com>
2015-02-08 16:56:48 +0100

    add class FileReader and FileSender - but not finished

--- src/Server/FileReceiver.java
+++ src/Server/FileReceiver.java
@@ -1,5 +1,44 @@
 package Server;
 
-public class FileReceiver {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class FileReceiver implements Runnable {
+
+	public static final int port = 1717;
+	public Socket receiver;
+
+	public static void main(String[] args) {
+		try {
+			ServerSocket listener = new ServerSocket(port);
+			
+			while (true){
+				FileReceiver fileRec = new FileReceiver();
+				fileRec.receiver = listener.accept();
+				new Thread(fileRec).start();
+				
+			}
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void run() {
+	
+		try {
+			InputStream in = receiver.getInputStream();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
 
 }
--- src/Server/FileSender.java
+++ src/Server/FileSender.java
@@ -1,5 +1,28 @@
 package Server;
 
-public class FileSender {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class FileSender {
+	public static final int port = 1717;
+	private static final String host = "localhost";
+	
+	public static void main(String[] args) {
+		
+		try {
+			Socket sender =new Socket(host, port);
+			OutputStream out = sender.getOutputStream();
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
